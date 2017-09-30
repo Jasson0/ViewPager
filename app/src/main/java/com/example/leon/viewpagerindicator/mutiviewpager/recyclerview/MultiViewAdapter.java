3a47c771e7bff5bf6c8127604943a9de3555c1a4
rdwang <rdwang@Ctrip.com>
2017-09-30 17:40:01 +0800

    添加注释，用以理解访问者模式

--- app/src/main/java/com/example/leon/viewpagerindicator/mutiviewpager/recyclerview/MultiViewAdapter.java
+++ app/src/main/java/com/example/leon/viewpagerindicator/mutiviewpager/recyclerview/MultiViewAdapter.java
@@ -18,6 +18,17 @@ import java.util.List;
 
 /**
  * Created by leon on 2017/9/14.
+ * ●　　抽象访问者(Visitor)角色：声明了一个或者多个方法操作，形成所有的具体访问者角色必须实现的接口。（对应typeFactory）
+
+ 　●　　具体访问者(ConcreteVisitor)角色：实现抽象访问者所声明的接口，也就是抽象访问者所声明的各个访问操作。（对应typeFactoryList）
+
+ 　●　　抽象节点(Node)角色：声明一个接受操作，接受一个访问者对象作为一个参数。(对应visitableModel)
+
+ 　●　　具体节点(ConcreteNode)角色：实现了抽象节点所规定的接受操作。（对应其他实现了对应visitableModel的model）
+
+ 　●　　结构对象(ObjectStructure)角色：有如下的责任，可以遍历结构中的所有元素；
+ 如果需要，提供一个高层次的接口让访问者对象可以访问每一个元素；如果需要，可以设计成一个复合对象或者一个聚集，
+ 如List或Set。（holder中的类）
  */
 
 public class MultiViewAdapter extends RecyclerView.Adapter<BaseViewHolder> {
@@ -58,6 +69,12 @@ public class MultiViewAdapter extends RecyclerView.Adapter<BaseViewHolder> {
     public MultiViewAdapter(Context context, List<VisitableModel> dataList) {
         this.models = dataList;
         this.context = context;
+        /**
+         * 通过接口抽象，将所有与列表相关的Model抽象为Visitable，当我们在初始化数据源时就能以List<Visitable>的形式将不同类型的Model集合在列表中；
+         *通过访问者模式，将列表类型判断的相关代码抽取到TypeFactoryForList 中，同时所有列表类型对应的布局资源都在这个类中进行管理与维护，以这样的方式巧妙的增强了扩展性与可维护性；
+         *getItemViewType中不再需要进行if判断，通过数据源控制列表的布局类型，同时返回的不再是简单的布局类型标识，而是布局的资源ID（通过modelList.get(position).type()获取），
+         *进一步简化代码（在onCreateViewHolder中会体现出来）；
+         */
         this.typeFactory = new TypeFactoryForList();
     }
 
